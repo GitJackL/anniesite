33e96c6a970bf4f257ce5573c151de5eb0588a03
Jack <116763735+GitJackL@users.noreply.github.com>
2024-08-04 15:38:39 +0100

    Fix file mistake

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-body {
-  padding-top: 56px;
-}
-
-.jumbotron {
-  background-image: url('your-image.jpg'); /* Replace with your image path */
-  background-size: cover;
-  color: white;
-}
-
-.footer {
-  padding: 1rem 0;
-  text-align: center;
-}
+A ChannelMergerNode
